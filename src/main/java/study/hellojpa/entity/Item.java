15e944d44ced22b9bbf6f9a6c5f943038792922d
SoohoLee <hyeri0603@naver.com>
2023-05-19 21:36:49 +0900

    project init

---
+++ src/main/java/study/hellojpa/entity/Item.java
@@ -0,0 +1,15 @@
+package study.hellojpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    private int price;
+}
---
+++ src/test/java/study/hellojpa/HelloJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.hellojpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
