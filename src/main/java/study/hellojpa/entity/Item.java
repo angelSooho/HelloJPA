0e3d611d7db4cfce0a74b272086d0af4581bc08e
SoohoLee <hyeri0603@naver.com>
2023-05-19 22:40:14 +0900

    05.19 테이블 전략 공부

--- src/main/java/study/hellojpa/entity/Item.java
+++ src/main/java/study/hellojpa/entity/Item.java
@@ -1,15 +1,32 @@
 package study.hellojpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@DiscriminatorColumn
+public abstract class Item {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     private int price;
 }
